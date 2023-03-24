1d2f9a7366c67ad89ebcd88985cf4dba67da729f
Johnny Jurak <johny9020@hotmail.com>
2023-03-24 21:21:49 +0100

    fucked up

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-msg = "Chello word"
+import pygame
 
-print(msg)
+def main():
+    pygame.init()
+    SCREEN = pygame.display.set_mode((640, 480))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+    SCREEN.fill((0, 255, 255))
+    pygame.display.flip()
+
+main()
