0f54345e48dc65d392017305edd235c7dba7492c
Johnny Jurak <johny9020@hotmail.com>
2023-03-24 22:12:28 +0100

    Fixed Stuff

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pygame
+from random import randrange
 
 def main(): 
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
     pygame.display.set_caption("Backgammon")
-    test = pygame.image.load("Assets/Board1.jpg")
+    board = pygame.image.load("Assets/Board.jpg")
 
-    test = pygame.transform.scale(test, (1280, 720))
+    board = pygame.transform.scale(board, (1280, 720))
+    print(randrange(0, 6))
+
+    font = pygame.font.SysFont('monaco', 24)
+    img = font.render(generateNumber(), True, (0, 0, 0))
+    img2 = font.render(generateNumber(), True, (0, 0, 0))
 
 
     running = True
@@ -15,10 +21,20 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                img = font.render(generateNumber(), True, (0, 0, 0))
+                img2 = font.render(generateNumber(), True, (0, 0, 0))
+                pygame.display.update()
 
         pygame.display.flip()
-        screen.blit(test, (0, 0))
+        screen.blit(board, (0, 0))
+        screen.blit(img, (0, 0))
+        screen.blit(img2, (0, 20))
 
     pygame.quit()
+
+
+def generateNumber():
+    return str(randrange(1, 7))
 
 main()
